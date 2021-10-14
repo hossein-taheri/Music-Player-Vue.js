8942727d5913c3f4b6019a468815cca713cd0244
Hossein <you@example.com>
2021-10-15 02:31:04 +0330

    Artist index has been added

--- src/pages/artist/index.vue
+++ src/pages/artist/index.vue
@@ -1,6 +1,83 @@
 <template>
-
+  <q-card
+    class="q-card-background q-mx-lg q-my-md row"
+    flat
+  >
+    <ArtistCard
+      v-for="(artist,index) in artists"
+      :key="index"
+      :artist="artist"
+    />
+  </q-card>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination
+      v-model="current"
+      color="white"
+      textColor="black"
+      :max="pageCount"
+      :max-pages="6"
+      :boundary-numbers="false"
+    />
+  </div>
 </template>
 <script>
-export default {}
+import RequestHelper from "src/helper/request";
+import HorizontalListItems from "components/HorizontalListItems";
+import ArtistCard from "components/ArtistCard";
+
+
+export default {
+  data() {
+    return {
+      artists: [],
+      current: 0,
+      pageCount: 0,
+    }
+  },
+  components: {
+    HorizontalListItems,
+    ArtistCard
+  },
+  watch: {
+    current() {
+      this.$route.params.page = this.current;
+      this.getArtistsData();
+    }
+  },
+  mounted() {
+    this.current = this.$route.params.page || 1;
+    this.getArtistsData();
+  },
+  methods: {
+    getArtistsData() {
+      let query = {
+        page: this.current,
+      };
+
+      this.artists = [];
+
+      RequestHelper
+        .send(
+          'get',
+          'artist/index',
+          {},
+          query
+        )
+        .then(data => {
+          this.artists = data.artists;
+          this.pageCount = data.pageCount;
+          this.current = data.page;
+        })
+        .catch(err => {
+
+        })
+    }
+  }
+
+}
 </script>
+<style>
+.q-card-background {
+  background: #00000000;
+}
+</style>
