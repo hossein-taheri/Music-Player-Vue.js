8ef91a3e37720aae7592ec648543e888160c375b
Hossein <you@example.com>
2021-10-15 00:56:32 +0330

    Album index has been added

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-card
+    @click="pushTo"
+    class="card q-mr-sm">
+    <q-img
+      class="image"
+      :src="image"
+    />
+
+    <div class="q-mx-sm text-white text-center text-caption">
+      <div>
+        {{ album.name }}
+      </div>
+      <div>
+        {{ album.year }}
+      </div>
+    </div>
+  </q-card>
+</template>
+
+<script>
+import request from "src/helper/request";
+
+export default {
+  props: [
+    'album'
+  ],
+  data() {
+    return {
+      image: null
+    }
+  },
+  mounted() {
+    this.image = request.BASE_URL + this.album.image;
+  },
+  methods: {
+    pushTo() {
+      this.$router.push({
+        name: 'album.edit',
+        params: {
+          album: this.album.id
+        }
+      })
+    }
+  }
+}
+</script>
+<style>
+.card {
+  width: 150px;
+  height: 220px;
+  background: #1E1E1E;
+}
+
+.image {
+  width: 150px;
+  height: 180px;
+}
+</style>
--- src/pages/album/index.vue
+++ src/pages/album/index.vue
@@ -1,6 +1,83 @@
 <template>
-
+  <q-card
+    class="q-card-background q-mx-lg q-my-md row"
+    flat
+  >
+    <AlbumCard
+      v-for="(album,index) in albums"
+      :key="index"
+      :album="album"
+    />
+  </q-card>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination
+      v-model="current"
+      color="white"
+      textColor="black"
+      :max="pageCount"
+      :max-pages="6"
+      :boundary-numbers="false"
+    />
+  </div>
 </template>
 <script>
-export default {}
+import RequestHelper from "src/helper/request";
+import HorizontalListItems from "components/HorizontalListItems";
+import AlbumCard from "components/AlbumCard";
+
+
+export default {
+  data() {
+    return {
+      albums: [],
+      current: 0,
+      pageCount: 0,
+    }
+  },
+  components: {
+    HorizontalListItems,
+    AlbumCard
+  },
+  watch: {
+    current() {
+      this.$route.params.page = this.current;
+      this.getAlbumsData();
+    }
+  },
+  mounted() {
+    this.current = this.$route.params.page || 1;
+    this.getAlbumsData();
+  },
+  methods: {
+    getAlbumsData() {
+      let query = {
+        page: this.current,
+      };
+
+      this.albums = [];
+
+      RequestHelper
+        .send(
+          'get',
+          'album/index',
+          {},
+          query
+        )
+        .then(data => {
+          this.albums = data.albums;
+          this.pageCount = data.pageCount;
+          this.current = data.page;
+        })
+        .catch(err => {
+
+        })
+    }
+  }
+
+}
 </script>
+<style>
+.q-card-background {
+  background: #00000000;
+}
+</style>
